1b35fa66f9d9413622439f75b63d08c2a77be890
azyiz <115402350+azyiz@users.noreply.github.com>
2023-12-06 00:45:49 +0100

    up

--- src/main/java/com/example/springbootesprit/repositories/FoyerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springbootesprit.repositories;
-
-import com.example.springbootesprit.entities.Foyer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FoyerRepository extends JpaRepository<Foyer,Long> {
-
-}
--- src/main/java/com/example/springbootesprit/repositories/ReservationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springbootesprit.repositories;
-
-import com.example.springbootesprit.entities.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationRepository extends JpaRepository<Reservation, String> {
-}
